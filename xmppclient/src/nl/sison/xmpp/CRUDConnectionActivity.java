69d47858d76ab92cb11ac1c7a3751e248f29eb1c
Jasm Sison <jasm.sison@gmail.com>
2012-05-20 19:45:08 +0200

    finished impleming pretest configuration validity before saving to database

--- xmppclient/src/nl/sison/xmpp/CRUDConnectionActivity.java
+++ xmppclient/src/nl/sison/xmpp/CRUDConnectionActivity.java
@@ -1,10 +1,16 @@
 package nl.sison.xmpp;
 
+import org.jivesoftware.smack.ConnectionConfiguration;
+import org.jivesoftware.smack.XMPPConnection;
+import org.jivesoftware.smack.XMPPException;
+
 import nl.sison.xmpp.dao.ConnectionConfigurationEntity;
 import nl.sison.xmpp.dao.ConnectionConfigurationEntityDao;
 import nl.sison.xmpp.dao.ConnectionConfigurationEntityDao.Properties;
 import nl.sison.xmpp.dao.DaoSession;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,7 +25,7 @@ public class CRUDConnectionActivity extends Activity {
 	public static final String RESTART_CONNECTION = "asnehnaoseuthaoseuthaoseuth2234";
 
 	private final String TAG = "CRUDConnectionActivity";
-	
+
 	private Long conn_config_id = (long) 0;
 
 	@Override
@@ -27,40 +33,41 @@ public class CRUDConnectionActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		View list_view = LayoutInflater.from(this).inflate(
 				R.layout.edit_connection, null, false);
-		if (isCreateConnection()) {
-			// TODO - show custom dialog with preset values, set hints in
-			// TextView for each provider (google talk, facebook, meebo etc.)
-		} else {
-			// TODO load settings from database and show them
-			long ccid = getIntent().getExtras().getLong(
-					ConnectionListActivity.KEY_CONNECTION_INDEX);
-
-			makeToast("intent extra " + ccid);
-
-			ConnectionConfigurationEntity cc = DatabaseUtil
-					.getReadOnlyDatabaseSession(this)
-					.getConnectionConfigurationEntityDao().queryBuilder()
-					.where(Properties.Id.eq(ccid)).list().get(0);
-			
-			conn_config_id = cc.getId();
-
-			setTextViewData(list_view, R.id.conn_label, cc.getLabel());
-			setTextViewData(list_view, R.id.conn_port, cc.getPort());
-			setTextViewData(list_view, R.id.conn_server, cc.getServer());
-			setTextViewData(list_view, R.id.conn_username, cc.getUsername());
-			setTextViewData(list_view, R.id.conn_password, cc.getPassword());
-			setTextViewData(list_view, R.id.conn_domain, cc.getDomain());
-//			setTextViewData(list_view, R.id.conn_jid, cc.getJid());
-			setTextViewData(list_view, R.id.conn_resource, cc.getResource());
-
-			setToggleButtonState(list_view, R.id.conn_encrypted, cc.getEncrypted());
-			setToggleButtonState(list_view, R.id.conn_compressed, cc.getCompressed());
-			setToggleButtonState(list_view, R.id.conn_sasl_authenticated, cc.getSaslauthenticated());
-
-			DatabaseUtil.close();
+		if (isExtantConnection()) {
+			showValuesFromDatabase(list_view);
 		}
 		setButtons(list_view);
 		setContentView(list_view);
+	}
+
+	private void showValuesFromDatabase(View list_view) {
+		long ccid = getIntent().getExtras().getLong(
+				ConnectionListActivity.KEY_CONNECTION_INDEX);
+
+		makeToast("intent extra " + ccid);
+
+		ConnectionConfigurationEntity cc = DatabaseUtil
+				.getReadOnlyDatabaseSession(this)
+				.getConnectionConfigurationEntityDao().queryBuilder()
+				.where(Properties.Id.eq(ccid)).list().get(0);
+
+		conn_config_id = cc.getId();
+
+		setTextViewData(list_view, R.id.conn_label, cc.getLabel());
+		setTextViewData(list_view, R.id.conn_port, cc.getPort());
+		setTextViewData(list_view, R.id.conn_server, cc.getServer());
+		setTextViewData(list_view, R.id.conn_username, cc.getUsername());
+		setTextViewData(list_view, R.id.conn_password, cc.getPassword());
+		setTextViewData(list_view, R.id.conn_domain, cc.getDomain());
+		setTextViewData(list_view, R.id.conn_resource, cc.getResource());
+
+		setToggleButtonState(list_view, R.id.conn_encrypted, cc.getEncrypted());
+		setToggleButtonState(list_view, R.id.conn_compressed,
+				cc.getCompressed());
+		setToggleButtonState(list_view, R.id.conn_sasl_authenticated,
+				cc.getSaslauthenticated());
+
+		DatabaseUtil.close();
 	}
 
 	private void setTextViewData(View parent, int view_id, String value) {
@@ -73,60 +80,121 @@ public class CRUDConnectionActivity extends Activity {
 		final Button button = (Button) parentView.findViewById(button_id);
 		button.setOnClickListener(listener);
 	}
-	
-	private void setToggleButtonState(View parentView, int button_id, boolean b)
-	{
-		ToggleButton tb = (ToggleButton) parentView
-				.findViewById(button_id);
-		tb.setChecked(b);		
+
+	private void setToggleButtonState(View parentView, int button_id, boolean b) {
+		ToggleButton tb = (ToggleButton) parentView.findViewById(button_id);
+		tb.setChecked(b);
 	}
 
 	private void setButtons(final View parent) {
 
-		setButtonListener(parent, R.id.conn_compressed, new View.OnClickListener() {
-			public void onClick(View v) {
-				ToggleButton tb = (ToggleButton) v;
-				if (tb.isChecked()) {
-					makeToast("connection will be compressed");
-				} else {
-					makeToast("connection will NOT be compressed");
-				}
-			}
-		});
-		setButtonListener(parent, R.id.conn_sasl_authenticated, new View.OnClickListener() {
-			public void onClick(View v) {
-				ToggleButton tb = (ToggleButton) v;
-				if (tb.isChecked()) {
-					makeToast("connection will be sasl_authenticated");
-				} else {
-					makeToast("connection will NOT be sasl_authenticated");
-				}
-			}
-		});		
-		setButtonListener(parent, R.id.conn_encrypted, new View.OnClickListener() {
-			public void onClick(View v) {
-				ToggleButton tb = (ToggleButton) v;
-				if (tb.isChecked()) {
-					makeToast("connection will be encrypted");
-					makeToast("Warning: encryption on this version of smack lib is most likely broken.");
-				} else {
-					makeToast("connection will NOT be encrypted");
-				}
-			}
-		});
-		
-		setButtonListener(parent, R.id.conn_test_and_save, new View.OnClickListener() {
-			public void onClick(View v) {
-				storeConnectionConfiguration(parent);
-				// TODO try catch and do some error checking here, e.g. unique
-				Intent restartConnectionOnService = new Intent(CRUDConnectionActivity.this, XMPPService.class);
-				restartConnectionOnService.putExtra(RESTART_CONNECTION, conn_config_id);
-				startService(restartConnectionOnService);
-				finish();
-			}
+		setButtonListener(parent, R.id.conn_compressed,
+				new View.OnClickListener() {
+					public void onClick(View v) {
+						ToggleButton tb = (ToggleButton) v;
+						if (tb.isChecked()) {
+							makeToast("connection will be compressed");
+						} else {
+							makeToast("connection will NOT be compressed");
+						}
+					}
+				});
+		setButtonListener(parent, R.id.conn_sasl_authenticated,
+				new View.OnClickListener() {
+					public void onClick(View v) {
+						ToggleButton tb = (ToggleButton) v;
+						if (tb.isChecked()) {
+							makeToast("connection will be sasl_authenticated");
+						} else {
+							makeToast("connection will NOT be sasl_authenticated");
+						}
+					}
+				});
+		setButtonListener(parent, R.id.conn_encrypted,
+				new View.OnClickListener() {
+					public void onClick(View v) {
+						ToggleButton tb = (ToggleButton) v;
+						if (tb.isChecked()) {
+							makeToast("connection will be encrypted");
+							makeToast("Warning: encryption on this version of smack lib is most likely broken.");
+						} else {
+							makeToast("connection will NOT be encrypted");
+						}
+					}
+				});
 
-		}); // TODO - merge test and save
+		setButtonListener(parent, R.id.conn_test_and_save,
+				new View.OnClickListener() {
+					public void onClick(View v) {
+						ConnectionConfigurationEntity conn_conf = getConnectionDetails(parent);
+						XMPPConnection connection = connectToServer(conn_conf);
+						if (connection != null && connection.isConnected()
+								&& connection.isAuthenticated()) {
+							storeConnectionConfiguration(conn_conf);
+							Intent restartConnectionOnService = new Intent(
+									CRUDConnectionActivity.this,
+									XMPPService.class);
+							restartConnectionOnService.putExtra(
+									RESTART_CONNECTION, conn_config_id);
+							startService(restartConnectionOnService);
+							finish();
+						} else {
+							createWarningConnectionBadDialog(getString(R.string.conn_bad_conn_conf));
+						}
 
+					}
+
+				});
+	}
+
+	private void createWarningConnectionBadDialog(String message) {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage(message)
+				.setCancelable(false)
+				.setPositiveButton(R.string.create_connection,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								View parent = LayoutInflater.from(
+										CRUDConnectionActivity.this).inflate(
+										R.layout.edit_connection, null, false);
+								ConnectionConfigurationEntity conn_conf = getConnectionDetails(parent);
+								storeConnectionConfiguration(conn_conf);
+								makeToast("stored bad configuration");
+								finish();
+							}
+						})
+				.setNegativeButton(android.R.string.cancel,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int which) {
+								makeToast("cancel store bad configuration");
+							}
+						});
+		builder.create().show();
+	}
+
+	private XMPPConnection connectToServer(ConnectionConfigurationEntity cc) {
+		ConnectionConfiguration xmpp_conn_config = new ConnectionConfiguration(
+				cc.getServer(), Integer.valueOf(cc.getPort()), cc.getDomain());
+		xmpp_conn_config.setCompressionEnabled(cc.getCompressed());
+		xmpp_conn_config
+				.setSASLAuthenticationEnabled(cc.getSaslauthenticated());
+		// xmpp_conn_config.setReconnectionAllowed(true); // TODO
+		XMPPConnection connection = new XMPPConnection(xmpp_conn_config);
+		try {
+			connection.connect();
+			makeToast(cc.getLabel() + "is connected:"
+					+ connection.isConnected());
+			connection.login(cc.getUsername(), cc.getPassword(),
+					cc.getResource());
+			makeToast(cc.getLabel() + "is authenticated:"
+					+ connection.isAuthenticated());
+		} catch (XMPPException e) {
+			connection = null;
+			e.printStackTrace();
+			Log.e(TAG, e.toString());
+		}
+		return connection;
 	}
 
 	private String extractStringFromTextView(final View parent, int view_id) {
@@ -134,13 +202,19 @@ public class CRUDConnectionActivity extends Activity {
 		return tv.getText().toString();
 	}
 
-	private void storeConnectionConfiguration(View parent) {
+	private void storeConnectionConfiguration(
+			ConnectionConfigurationEntity conn_config) {
 		DaoSession daoSession = DatabaseUtil.getWriteableDatabaseSession(this);
-		ConnectionConfigurationEntity conn_config = new ConnectionConfigurationEntity();
 		ConnectionConfigurationEntityDao conn_config_dao = daoSession
 				.getConnectionConfigurationEntityDao();
-		
-		if(conn_config_id != 0)
+		conn_config_dao.insertOrReplace(conn_config);
+		DatabaseUtil.close();
+	}
+
+	private ConnectionConfigurationEntity getConnectionDetails(View parent) {
+		ConnectionConfigurationEntity conn_config = new ConnectionConfigurationEntity();
+
+		if (conn_config_id != 0)
 			conn_config.setId(conn_config_id);
 
 		conn_config.setConnection_success(0);
@@ -148,16 +222,12 @@ public class CRUDConnectionActivity extends Activity {
 		ToggleButton tb = (ToggleButton) parent
 				.findViewById(R.id.conn_sasl_authenticated);
 		conn_config.setSaslauthenticated(tb.isChecked());
-		
-		tb = (ToggleButton) parent
-				.findViewById(R.id.conn_compressed);
-		conn_config.setCompressed(tb.isChecked());
-		
-		tb = (ToggleButton) parent
-				.findViewById(R.id.conn_encrypted);
-		conn_config.setEncrypted(tb.isChecked());		
 
-//		conn_config.setJid(extractStringFromTextView(parent, R.id.conn_jid));
+		tb = (ToggleButton) parent.findViewById(R.id.conn_compressed);
+		conn_config.setCompressed(tb.isChecked());
+
+		tb = (ToggleButton) parent.findViewById(R.id.conn_encrypted);
+		conn_config.setEncrypted(tb.isChecked());
 
 		conn_config
 				.setLabel(extractStringFromTextView(parent, R.id.conn_label));
@@ -165,8 +235,9 @@ public class CRUDConnectionActivity extends Activity {
 		conn_config.setPassword(extractStringFromTextView(parent,
 				R.id.conn_password)); // TODO encrypt using user's master
 										// password
-		
-		conn_config.setDomain(extractStringFromTextView(parent, R.id.conn_domain));
+
+		conn_config.setDomain(extractStringFromTextView(parent,
+				R.id.conn_domain));
 
 		conn_config.setPort(extractStringFromTextView(parent, R.id.conn_port));
 
@@ -178,17 +249,12 @@ public class CRUDConnectionActivity extends Activity {
 
 		conn_config.setUsername(extractStringFromTextView(parent,
 				R.id.conn_username));
-
-		// TODO - set record id in class field
-		conn_config_dao.insertOrReplace(conn_config);
-
-		DatabaseUtil.close();
-
+		return conn_config;
 	}
 
-	private boolean isCreateConnection() {
+	private boolean isExtantConnection() {
 		String key_connection_index = ConnectionListActivity.KEY_CONNECTION_INDEX;
-		return !getIntent().hasExtra(key_connection_index);
+		return getIntent().hasExtra(key_connection_index);
 	}
 
 	private void makeToast(String message) {
