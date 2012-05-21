f47d035079bfb8e8e0515d6d71e162c849ab75c1
Jasm Sison <jasm.sison@gmail.com>
2012-05-21 07:39:17 +0200

    start XMPPService from boot

---
+++ xmppclient/src/nl/sison/xmpp/StartXMPPServiceReceiver.java
@@ -0,0 +1,15 @@
+package nl.sison.xmpp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class StartXMPPServiceReceiver extends BroadcastReceiver {
+
+	@Override
+	public void onReceive(Context context, Intent i) {
+		Intent service = new Intent(context, XMPPService.class);
+		context.startService(service);
+	}
+
+}
