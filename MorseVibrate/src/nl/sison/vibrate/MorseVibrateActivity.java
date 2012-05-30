4d82f05d772e86452d03c11762b031c158ae3330
Jasm Sison <jasm.sison@gmail.com>
2012-05-30 12:41:07 +0200

    added new projects

---
+++ MorseVibrate/src/nl/sison/vibrate/MorseVibrateActivity.java
@@ -0,0 +1,13 @@
+package nl.sison.vibrate;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MorseVibrateActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
---
+++ QRLabelArt/src/nl/sison/qr/QRLabelArtActivity.java
@@ -0,0 +1,13 @@
+package nl.sison.qr;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class QRLabelArtActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
