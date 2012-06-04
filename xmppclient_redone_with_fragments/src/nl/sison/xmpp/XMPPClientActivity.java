c47cde596b9a6ed256991b7af84366f85249ca2e
Jasm Sison <buggasnoo@gmail.com>
2012-06-04 15:07:57 +0200

    fragments are a go

---
+++ xmppclient_redone_with_fragments/src/nl/sison/xmpp/XMPPClientActivity.java
@@ -0,0 +1,13 @@
+package nl.sison.xmpp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class XMPPClientActivity extends FragmentActivity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
